5fcfa2d7281f71b60803e078750892f58933d7fd
Mirko <ruhlmirkojoel@gmail.com>
2024-11-18 23:10:55 -0300

    Added switch theory

---
+++ more-JS/app.js
@@ -0,0 +1,20 @@
+// Switch statements
+
+let new_number = Number(prompt("What number do you want to print?"));
+
+console.log(typeof new_number);
+
+switch (new_number) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+}
