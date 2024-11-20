ceee5d32dd0054412c5771ae42470dddb2c80718
Mirko <ruhlmirkojoel@gmail.com>
2024-11-20 19:14:43 -0300

    First version of the game

--- dom-and-events/script.js
+++ dom-and-events/script.js
@@ -8,8 +8,27 @@
 //document.querySelector('.number').textContent = 13;
 //document.querySelector('.score').textContent = 0;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 function check_value() {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(secretNumber);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Add a valid number!';
+  } else if (secretNumber === guess) {
+    document.querySelector('h1').textContent = 'You won!';
+    document.querySelector('body').style.backgroundColor = 'green';
+  } else {
+    let diff = Math.abs(secretNumber - guess);
+    if (diff <= 5) {
+      document.querySelector('.message').textContent = 'Very Warm';
+    } else if (diff <= 10) {
+      document.querySelector('.message').textContent = 'Warm';
+    } else {
+      document.querySelector('.message').textContent = 'Very Cold';
+    }
+  }
 }
 
 document.querySelector('.check').addEventListener('click', check_value);
