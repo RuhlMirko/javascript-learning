1059249d4ce4380e3c8920a0b898e85e65ad8b0a
Mirko <ruhlmirkojoel@gmail.com>
2024-11-19 22:37:55 -0300

    Initial version of the page

--- dom-and-events/script.js
+++ dom-and-events/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
-let class_selection = document.querySelector('.message');
+//let class_selection = (document.querySelector('.message').textContent =
+// 'Correct number');
 
-console.log(class_selection);
+//console.log(class_selection);
+
+//document.querySelector('.number').textContent = 13;
+//document.querySelector('.score').textContent = 0;
+
+function check_value() {
+  console.log(document.querySelector('.guess').value);
+}
+
+document.querySelector('.check').addEventListener('click', check_value);
