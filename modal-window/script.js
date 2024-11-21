ddbe75ea7eeb78e29b286bb67633e1b0e37843e4
Mirko <ruhlmirkojoel@gmail.com>
2024-11-20 22:18:52 -0300

    Added functionality for adding and removing classes

--- modal-window/script.js
+++ modal-window/script.js
@@ -6,3 +6,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
 console.log(btnShowModal);
+
+function remove_hidden_class() {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+  btnCloseModal.addEventListener('click', add_hidden_class);
+}
+
+function add_hidden_class() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', remove_hidden_class);
+}
