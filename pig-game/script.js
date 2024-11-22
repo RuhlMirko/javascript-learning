f4a4157b434714836cf56b340849d65ab8b70835
Mirko <ruhlmirkojoel@gmail.com>
2024-11-22 09:31:48 -0300

    Current score updates player score when rolling one

--- pig-game/script.js
+++ pig-game/script.js
@@ -4,22 +4,40 @@
 const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 const newBtn = document.querySelector('.btn--new');
-let current_p1 = document.getElementById('current--0').textContent;
-let current_p2 = document.getElementById('current--1').textContent;
 
-let score_p1 = (document.getElementById('score--0').textContent = 0);
-let score_p2 = (document.getElementById('score--1').textContent = 0);
+let score_p1 = document.getElementById('score--0');
+let score_p2 = document.getElementById('score--1');
+score_p1.textContent = 0;
+score_p2.textContent = 0;
+
 const p1 = document.querySelector('.player--0');
 const p2 = document.querySelector('.player--1');
 let diceImg = document.querySelector('img');
 
+const total_scores = [0, 0];
+let currentScore = 0;
+let active_player = 0;
+
 function roll_dice() {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceImg.src = `dice-${dice}.png`;
-  score_p1 += dice;
+
   if (dice === 1) {
     p1.classList.toggle('player--active');
     p2.classList.toggle('player--active');
+
+    document.getElementById(`score--${active_player}`).textContent =
+      currentScore;
+
+    currentScore = 0;
+    document.getElementById(`current--${active_player}`).textContent =
+      currentScore;
+    active_player = active_player == 0 ? 1 : 0;
+  } else {
+    currentScore += dice;
+    //active_player = p1.classList.contains('player-active') ? 0 : 1;
+    document.getElementById(`current--${active_player}`).textContent =
+      currentScore;
   }
 }
 
