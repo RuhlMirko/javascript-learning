0d94c021a696316a3f5e93a86dc7de8ca41203e1
Mirko <ruhlmirkojoel@gmail.com>
2024-11-25 21:01:42 -0300

    Saving values from array to new individual values

--- section-9/script.js
+++ section-9/script.js
@@ -27,3 +27,13 @@ const restaurant = {
     },
   },
 };
+
+// Long way
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+// Short way
+const [x, y, z] = arr;
+console.log(a, b, c);
+console.log(x, y, z);
