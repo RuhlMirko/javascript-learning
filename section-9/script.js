d6e95051f7123e0bb5d53ca58ce0e1a493ab75c1
Mirko <ruhlmirkojoel@gmail.com>
2024-11-27 18:14:21 -0300

    ES6 new features for objects

--- section-9/script.js
+++ section-9/script.js
@@ -5,6 +5,23 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[2]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   restau_name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -18,47 +35,37 @@ const restaurant = {
   ],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
+  // ES6
+  openingHours,
+  // OLD way
+  //order: function (starterIndex, mainIndex) {
+  //return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  //},
+  // NEW Way
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 4,
-    mainIndex = 0,
-    time = '00:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 4, mainIndex = 0, time = '00:00', address }) {
     let output = `New delivery made at ${time} for ${address} street`;
     console.log(output);
     output = `Starter chosen: ${this.starterMenu[starterIndex]}, Main dish: ${this.mainMenu[mainIndex]}`;
     console.log(output);
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+
+console.log(restaurant.openingHours);
 
 /* For of Loop
 ///////////////////////////////////////
