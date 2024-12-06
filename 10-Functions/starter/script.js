992b37d540cf009d5b263c114300c300166d3f7d
Mirko <ruhlmirkojoel@gmail.com>
2024-12-06 17:28:12 -0300

    Added example of functions using outside scope variables

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,7 +1,27 @@
 'use strict';
 
-const bookingList = [];
+const flight = 'LH541';
+const jonas = {
+  firstName: 'Jonas Schmedtmann',
+  passport: 24739479281,
+};
 
+const checkIn = function (flightNum, passenger) {
+  (flightNum = 'LH999'), (passenger.firstName = 'Mr. ' + passenger.firstName);
+
+  console.log(
+    passenger.passport === 24739479281 ? 'Check in' : 'Invalid Passport'
+  );
+};
+checkIn(flight, jonas);
+jonas.passport = 2323333;
+jonas.firstName = 'Mirko Ruhl';
+checkIn(flight, jonas);
+console.log(flight, jonas);
+
+/* Functions deafult parameters
+//////////////////////////////////////
+const bookingList = [];
 const createBooking = function (
   flightNum,
   numPassengers = 1,
@@ -12,11 +32,10 @@ const createBooking = function (
     numPassengers,
     price,
   };
-
   bookingList.push(booking);
 };
-
 createBooking('LH20334', 405, 700);
 createBooking('WC15631');
-createBooking('LH19882', 105);
+createBooking('LH19882', undefined, 105); // Skips value with undefined
 console.log(bookingList);
+*/
