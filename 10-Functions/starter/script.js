d7a1d3c5e5944abcd168d05d7ab2a8376017eb04
Mirko <ruhlmirkojoel@gmail.com>
2024-12-06 12:59:43 -0300

    Added deafult parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bookingList = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  bookingList.push(booking);
+};
+
+createBooking('LH20334', 405, 700);
+createBooking('WC15631');
+createBooking('LH19882', 105);
+console.log(bookingList);
