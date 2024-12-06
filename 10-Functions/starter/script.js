3fbda1a2720c2da1e0632f337e6f1092d98aedba
Mirko <ruhlmirkojoel@gmail.com>
2024-12-06 18:43:50 -0300

    Added higher order functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,11 +1,28 @@
 'use strict';
 
+// Higher order functions
+const oneWord = function (userInput) {
+  return userInput.replaceAll(' ', '').toLowerCase();
+};
+const upperFirst = function (userInput) {
+  let [first, ...rest] = userInput.split(' ');
+  //first = first[0].toUpperCase() + first.slice(1).toLowerCase();
+  return [first.toUpperCase(), ...rest].join(' ');
+};
+const transformer = function (str, fn) {
+  console.log(`Transformed: ${fn(str)} by <${fn.name}()>`);
+};
+transformer('JavaScirpt is the best', upperFirst);
+transformer('programmed to work', oneWord);
+console.log(upperFirst('heLLO World programmed'));
+//console.log(oneWord('Hello world, programmed to work and not to feel'));
+
+/* Functions referencial and primitives values
 const flight = 'LH541';
 const jonas = {
   firstName: 'Jonas Schmedtmann',
   passport: 24739479281,
 };
-
 const checkIn = function (flightNum, passenger) {
   (flightNum = 'LH999'), (passenger.firstName = 'Mr. ' + passenger.firstName);
 
@@ -13,12 +30,16 @@ const checkIn = function (flightNum, passenger) {
     passenger.passport === 24739479281 ? 'Check in' : 'Invalid Passport'
   );
 };
+const newPass = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+  person.passport = String(person.passport).padStart(6, '99');
+};
 checkIn(flight, jonas);
-jonas.passport = 2323333;
 jonas.firstName = 'Mirko Ruhl';
+newPass(jonas);
 checkIn(flight, jonas);
 console.log(flight, jonas);
-
+*/
 /* Functions deafult parameters
 //////////////////////////////////////
 const bookingList = [];
