846ca0aa7fb60b00e55db12a35258cac8e2f18d0
Mirko <ruhlmirkojoel@gmail.com>
2024-12-07 18:19:05 -0300

    Added set Time out function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -31,6 +31,21 @@ f();
 h();
 f();
 
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(
+      `There are 3 groups, each with ${Math.trunc(perGroup)} passengers`
+    );
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(100, 3);
+
 /* Anonymus functions (Inmediatly Invoked Function Expression) or IIFE
 (function () {
   console.log('Run this time');
