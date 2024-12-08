af3d1f54a4fc8f29c1d93df08c685a65bebb67c7
Mirko <ruhlmirkojoel@gmail.com>
2024-12-07 22:41:27 -0300

    Added final version of the challenge

--- 11-Arrays/starter/script.js
+++ 11-Arrays/starter/script.js
@@ -109,12 +109,37 @@ GOOD LUCK 😀
 // 1
 const checkDogs = function (dogsJulia, dogsKate) {
   const fixJuliaDogs = dogsJulia.slice(1, -2);
-  fixJuliaDogs.forEach(function (dogAge) {
-    console.log(dogAge > 3 ? 'Adult' : 'Puppy');
+  const allDogs = fixJuliaDogs.concat(dogsKate);
+  allDogs.forEach(function (dogAge, index) {
+    console.log(
+      dogAge > 3
+        ? `Dog number ${index + 1} is an adult`
+        : `Dog number ${index + 1} is still a puppy`
+    );
   });
+  /*
+  fixJuliaDogs.forEach(function (dogAge, index) {
+    console.log(
+      dogAge > 3
+        ? `Dog number ${index + 1} is an adult`
+        : `Dog number ${index + 1} is still a puppy`
+    );
+  });
+  console.log('Kate Dogs: ');
+
+  dogsKate.forEach(function (dogAge, index) {
+    console.log(
+      dogAge > 3
+        ? `Dog number ${index + 1} is an adult`
+        : `Dog number ${index + 1} is still a puppy`
+    );
+  });
+  */
 };
 
+console.log('Test data 1:');
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log('\nTest data 2:');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 /////////////////////////////////////////////////
